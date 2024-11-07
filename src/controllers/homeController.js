ca4b6e44447c35fb9d7484febd58c9ca8f9a8e48
h5avlov <h5avlov@abv.bg>
2024-11-07 10:58:16 +0200

    Data layer to fetch and save data added

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,13 +1,22 @@
 import express from 'express'; 
+import { getAll, getById } from "../data/movieData.js"; 
 
 const router = express.Router(); 
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => { 
+    const movies = await getAll(); 
     res.render('home', { movies }); 
 }); 
 
 router.get('/about', (req, res) => {
     res.render('home/about'); 
+}); 
+
+router.get('/get/:id', async (req, res) => { 
+    const id = req.params.id; 
+    const movie = await getById(id); 
+
+    res.send(movie); 
 }); 
 
 export { router as homeControllerRouter }; 
---
+++ src/data/movieData.js
@@ -0,0 +1,29 @@
+import fs from "fs/promises"; 
+
+const dbPath = './src/db.json'; 
+
+const getDb = async function() { 
+    const data = await fs.readFile(dbPath, {encoding: 'utf-8'}); 
+    const json = JSON.parse(data); 
+
+    return json; 
+} 
+
+const saveDb = async function(movie) {
+    const json = await getDb(); 
+    json.push(movie); 
+    await fs.writeFile(dbPath, JSON.stringify(json, null, 2)); 
+} 
+
+const getById = async function(id) { 
+    const movies = await getDb(); 
+    const movie = movies.find((m) => m.id === parseInt(id)); 
+
+    return movie; 
+}; 
+
+const getAll = async function() {
+    return await getDb(); 
+}; 
+
+export { getById, getAll }; 
